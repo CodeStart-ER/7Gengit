71dc8f287899ea1418d6af6aaf8c59e703886f5d
CodeStart-ER <eddyrual@gmail.com>
2020-03-04 13:26:33 -0600

    2020/03/04 commiting several changes in local repo, among them it's been included practice #8 javascript

---
+++ p8hellokoders/js/main.js
@@ -0,0 +1,39 @@
+var string = "una cadena de caracteres";
+var number = 4;
+var object = {
+    name:"some name",
+    age:30,
+    phone:"5544332211"
+}
+
+var array = [
+    "koder a",
+    "koder b",
+    "koder c"
+]
+
+var numbersArray = [
+1,
+2,
+3
+
+]
+
+var someNumber = 34
+
+var crazyArray = [
+
+    "a",
+    "some string",
+    6,
+    {
+        some:"object"
+    }
+]
+
+
+
+console.log("Hello Koders");
+console.warn("aware koders");
+console.error("Noooo Koders");
+
