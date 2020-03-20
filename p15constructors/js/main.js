ef4a52eaac0c0174fde1186d5b59cdf558574a5a
CodeStart-ER <eddyrual@gmail.com>
2020-03-19 18:12:28 -0600

    2020/03/19 updating file p15constructors no changes made

--- p15constructors/js/main.js
+++ p15constructors/js/main.js
@@ -14,9 +14,23 @@ var kodersMatrix = [
 ]
         //arrayKoder = arr
 
-function koderObject(kodersMatrix) {
+/*function koderObject(kodersMatrix) {
     var rv = {};
     for (var i = 0; i < kodersMatrix.length; ++i)
       rv[i] = kodersMatrix[i];
     return rv;
   }
+*/
+
+var koder1 = {
+    
+    name: "israel",
+    lastName: "salinas martinez",
+    phone: 5543788096,
+    birthDate: "27-09-1989",
+
+
+}
+
+//hago matriz que saca de la matriz los elementos para crear los objetos, 1 por cada kodersMatrix
+
